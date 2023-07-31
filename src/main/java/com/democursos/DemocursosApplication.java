8db82308fbb30c13cbb2289cbab6b64e39ab2489
edgarciaguilar <edgarciaguilar@gmail.com>
2023-07-30 20:00:41 -0600

    archivos base para iniciar el proyecto DemoCursos2023

---
+++ src/main/java/com/democursos/DemocursosApplication.java
@@ -0,0 +1,13 @@
+package com.democursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/democursos/DemocursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.democursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
